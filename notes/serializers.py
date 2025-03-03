9ccb60a9ac50b343658bd9d1ea827b43315bf172
20242174-RALPH\admin <oloresralph@gmail.com>
2025-03-03 16:02:35 +1300

    Django cicd

---
+++ notes/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from notes.models import Note
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source="user.username")
+    class Meta:
+        model = Note
+        fields = ["id","title", "content", "user", "username"]
